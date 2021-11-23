5d5ebe451be54c996bea3c3c62dbf99a201a22df
Justine <hell.justine@gmail.com>
2021-11-23 15:58:27 +0100

    Stubbing Child Components

---
+++ tests/unit/MessageContainer.spec.js
@@ -0,0 +1,19 @@
+import MessageContainer from '@/components/MessageContainer'
+import { mount } from '@vue/test-utils'
+
+describe('MessageContainer', () => {
+  it('Wraps MessageDisplay component', async () => {
+    const wrapper = mount(MessageContainer, {
+      global: {
+        stubs: {
+          MessageDisplay: {
+            template: '<p data-testid="message">Hello from the db!</p>',
+          },
+        },
+      },
+    })
+
+    const message = wrapper.find('[data-testid="message"]').text()
+    expect(message).toEqual('Hello from the db!')
+  })
+})
